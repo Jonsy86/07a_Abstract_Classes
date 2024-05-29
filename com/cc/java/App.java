7fda260a4b0b90128a8c73f4554b78c4997b49db
Jonsy86 <jonasnolte86@gmail.com>
2024-05-29 11:33:58 +0200

    up

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,10 +4,34 @@ public class App {
     
     public static void main(String[] args) {
      
-       
+        Rectangle rectangle = new Rectangle(10.0, 10.0);
+        Circle circle = new Circle(5.0);
+        Triangle triangle = new Triangle(10.0,10.0);
+
+
+
+
+        output("------ Flächen ------"); 
+        output("Fläche Rechteck: " + rectangle.area());
+        output("Fläche Kreis: " + circle.area());
+        output("Fläche Dreieck: " + triangle.area());
+
+
+        output("Diff. r-t :" + areaDiff(rectangle, triangle));
+        output("Diff. c-r: " + areaDiff(circle,rectangle));
+        output("Diff. c-t: " + areaDiff(circle, triangle));
+        output("Diff. t-t: " + areaDiff(triangle, triangle));
+    
+
+
+
     }
 
   
+  // 2. Iteration: Polymorphie - besser! // Safe, Bro , safe !
+  private static double areaDiff(Shape s1, Shape s2){
+    return s1.area() - s2.area();
+}
 
    
 
