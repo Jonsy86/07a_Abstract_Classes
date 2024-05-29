7fdd864f1e27f1eb486f99c0e977e4869faf2646
Jonsy86 <jonasnolte86@gmail.com>
2024-05-29 09:31:26 +0200

    build classes

---
+++ com/cc/java/Rectangle.java
@@ -0,0 +1,16 @@
+package com.cc.java;
+
+public class Rectangle {
+
+  private double height;
+  private double width;
+
+  
+public Rectangle(double height, double width) {
+    this.height = height;
+    this.width = width;
+}
+  
+  
+
+}
---
+++ com/cc/java/Triangle.java
@@ -0,0 +1,15 @@
+package com.cc.java;
+
+public class Triangle {
+
+    private double baseline;
+    private double height;
+    
+    public Triangle(double baseline, double height) {
+        this.baseline = baseline;
+        this.height = height;
+    }
+
+    
+
+}
